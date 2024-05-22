bf21060341425b2be9777b7b4bb2b1a96f70acc4
andresds <andre.souza@asapcred.com.br>
2024-05-22 15:15:44 -0300

    setup form handleSubmit function

--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -1,9 +1,18 @@
+"use client"
+
 import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form"
 
 export function AuthForm() {
+  const authForm = useForm()
+
+  const handleSubmit = (data: any) => {
+    console.log(data)
+  }
+
   return (
     <div className="flex min-h-[100dvh] items-center justify-center bg-gray-100 px-4 dark:bg-gray-950">
       <Card className="w-full max-w-md">
@@ -12,12 +21,14 @@ export function AuthForm() {
           <CardDescription>Enter your email below to receive a magic link to sign in.</CardDescription>
         </CardHeader>
         <CardContent>
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit} className="space-y-4">
             <div>
               <Label className="sr-only" htmlFor="email">
                 Email
               </Label>
-              <Input className="w-full" id="email" placeholder="Enter your email" required type="email" />
+              <Input
+              {...authForm.register("email")}
+              className="w-full" id="email" placeholder="Enter your email" required type="email" />
             </div>
             <Button className="w-full" type="submit">
               Send Magic Link
